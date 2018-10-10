0e5f6f8dd9d964f5e3de2e4e0af56acfc51fd2ae
Dessertion <73731354pi@gmail.com>
2018-10-10 02:14:22 -0400

    Added Log tile outline

---
+++ src/com/Dessertion/src/Tiles/TileLog.java
@@ -0,0 +1,13 @@
+package com.Dessertion.src.Tiles;
+
+public class TileLog extends Tile {
+	
+	public static String fileString = "/tileLog.png";
+	public TileLog() {
+		super(fileString);
+	}
+	
+}
+
+
+
